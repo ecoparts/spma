d7b970c979c33bd852230c437098e82f643cf725
Nils Dralle <nils.91@googlemail.com>
2016-04-22 11:10:30 +0200

    [BLETest] Added permission to manifest
    Added btn click listeners
    
    Signed-off-by: Nils Dralle <nils.91@googlemail.com>

--- BLETest/app/src/main/java/de/dralle/bletest/MainActivity.java
+++ BLETest/app/src/main/java/de/dralle/bletest/MainActivity.java
@@ -2,6 +2,8 @@ package de.dralle.bletest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addButtonListeners();
+    }
+
+    private void addButtonListeners() {
+        Button btnBTCheck=(Button)findViewById(R.id.btnBTCheck);
+        Button btnBLECheck=(Button)findViewById(R.id.btnBLECheck);
+
+        btnBTCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnBLECheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
