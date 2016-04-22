86fcaa641d64236ad503389168c7558a02c72c05
Nils Dralle <nils.91@googlemail.com>
2016-04-22 11:38:51 +0200

    [BLETest] Added simple logger
    Signed-off-by: Nils Dralle <nils.91@googlemail.com>

--- BLETest/app/src/main/java/de/dralle/bletest/MainActivity.java
+++ BLETest/app/src/main/java/de/dralle/bletest/MainActivity.java
@@ -3,16 +3,20 @@ package de.dralle.bletest;
 import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static String LOG_TAG = MainActivity.class.getName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(LOG_TAG,"App created");
         addButtonListeners();
     }
 
@@ -25,9 +29,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
                     Toast.makeText(getApplicationContext(), R.string.btSupported, Toast.LENGTH_SHORT).show();
-
+                    Log.i(LOG_TAG, "Bluetooth supported");
                 }else{
                     Toast.makeText(getApplicationContext(), R.string.btNotSupported, Toast.LENGTH_SHORT).show();
+                    Log.w(LOG_TAG, "Bluetooth not supported");
                 }
             }
         });
@@ -36,9 +41,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
                     Toast.makeText(getApplicationContext(), R.string.bleSupported, Toast.LENGTH_SHORT).show();
+                    Log.i(LOG_TAG, "BLE supported");
 
                 }else{
                     Toast.makeText(getApplicationContext(), R.string.bleNotSupported, Toast.LENGTH_SHORT).show();
+                    Log.w(LOG_TAG, "BLE not supported");
                 }
             }
         });
