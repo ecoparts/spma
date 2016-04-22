a2ba738387f588aa3f060b15b184436f61e45dcd
Nils Dralle <nils.91@googlemail.com>
2016-04-22 11:27:18 +0200

    [BLETest] Added checks for BT and BLE
    Signed-off-by: Nils Dralle <nils.91@googlemail.com>

--- BLETest/app/src/main/java/de/dralle/bletest/MainActivity.java
+++ BLETest/app/src/main/java/de/dralle/bletest/MainActivity.java
@@ -1,9 +1,11 @@
 package de.dralle.bletest;
 
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,13 +23,23 @@ public class MainActivity extends AppCompatActivity {
         btnBTCheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
+                    Toast.makeText(getApplicationContext(), R.string.btSupported, Toast.LENGTH_SHORT).show();
 
+                }else{
+                    Toast.makeText(getApplicationContext(), R.string.btNotSupported, Toast.LENGTH_SHORT).show();
+                }
             }
         });
         btnBLECheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+                    Toast.makeText(getApplicationContext(), R.string.bleSupported, Toast.LENGTH_SHORT).show();
 
+                }else{
+                    Toast.makeText(getApplicationContext(), R.string.bleNotSupported, Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
