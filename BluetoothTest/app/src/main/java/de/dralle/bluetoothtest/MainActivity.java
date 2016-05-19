4a7cc11791a313691c66fbcdc082fdeac901deec
Nils Dralle <nils.91@googlemail.com>
2016-05-19 16:08:41 +0200

    [BluetoothTest] bound button to device discovery
    Signed-off-by: Nils Dralle <nils.91@googlemail.com>

--- BluetoothTest/app/src/main/java/de/dralle/bluetoothtest/MainActivity.java
+++ BluetoothTest/app/src/main/java/de/dralle/bluetoothtest/MainActivity.java
@@ -9,6 +9,8 @@ import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,6 +36,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnScan=(Button)findViewById(R.id.btnScn);
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startDeviceScan();
+            }
+        });
 
 
     }
