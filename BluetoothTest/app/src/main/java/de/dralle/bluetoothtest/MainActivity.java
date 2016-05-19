b999e305f2dc157927804c85bc7223a1736fa9d9
Nils Dralle <nils.91@googlemail.com>
2016-05-19 15:08:07 +0200

    [BluetoothTest] added check if bluetooth is supported
    and a toast
    
    Signed-off-by: Nils Dralle <nils.91@googlemail.com>

--- BluetoothTest/app/src/main/java/de/dralle/bluetoothtest/MainActivity.java
+++ BluetoothTest/app/src/main/java/de/dralle/bluetoothtest/MainActivity.java
@@ -1,13 +1,25 @@
 package de.dralle.bluetoothtest;
 
+import android.bluetooth.BluetoothAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String LOG_TAG=MainActivity.class.getName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothAdapter btAdapter=BluetoothAdapter.getDefaultAdapter();
+        if(btAdapter!=null){
+
+        }else{
+            Log.e(LOG_TAG,"Bluetooth not supported");
+            Toast.makeText(getApplicationContext(),getText(R.string.btNotSupported),Toast.LENGTH_LONG).show();
+            finish();
+        }
     }
 }
