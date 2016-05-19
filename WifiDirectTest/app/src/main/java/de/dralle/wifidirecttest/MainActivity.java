bebcc2b754236dbcc08685e0be7dde8f2e9d6732
Nils Dralle <nils.91@googlemail.com>
2016-05-19 14:39:55 +0200

    Revert "update"
    This reverts commit 4afb8829e832204fc9c6d8ef75acf9d422943a3f.

--- WifiDirectTest/app/src/main/java/de/dralle/wifidirecttest/MainActivity.java
+++ WifiDirectTest/app/src/main/java/de/dralle/wifidirecttest/MainActivity.java
@@ -1,7 +1,5 @@
 package de.dralle.wifidirecttest;
 
-import android.content.Context;
-import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,9 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        wp2pManager=(WifiP2pManager)getSystemService(Context.WIFI_P2P_SERVICE);
-
-        channel= WifiP2pManager.
     }
 }
