8216d3493e8cf39f7dd55c964e2b2a2ce90d8b2f
Nils Dralle <nils.91@googlemail.com>
2016-05-19 16:02:23 +0200

    [BluetoothTest] implemented device discovery
    Signed-off-by: Nils Dralle <nils.91@googlemail.com>

--- BluetoothTest/app/src/main/java/de/dralle/bluetoothtest/MainActivity.java
+++ BluetoothTest/app/src/main/java/de/dralle/bluetoothtest/MainActivity.java
@@ -1,7 +1,11 @@
 package de.dralle.bluetoothtest;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +15,21 @@ public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG=MainActivity.class.getName();
 
     private int REQUEST_ENABLE_BT=42;
+
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                Log.i(LOG_TAG,"New device found");
+                Log.i(LOG_TAG,device.getAddress());
+                Log.i(LOG_TAG,device.getName());
+                Log.i(LOG_TAG,device.getBluetoothClass().toString());
+                Log.i(LOG_TAG,"Bond state: "+device.getBondState()+"");
+                Log.i(LOG_TAG,"Type :"+device.getBondState()+"");
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
     }
     protected void onStart(){
         super.onStart();
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(broadcastReceiver, filter); // Don't forget to unregister during onDestroy
+
+
         BluetoothAdapter btAdapter=BluetoothAdapter.getDefaultAdapter();
         if(btAdapter!=null){
             if(!btAdapter.isEnabled()){
@@ -56,6 +79,8 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onDestroy(){
         super.onDestroy();
+        unregisterReceiver(broadcastReceiver);
+
         BluetoothAdapter btAdapter=BluetoothAdapter.getDefaultAdapter();
         if(btAdapter!=null&&btAdapter.isEnabled()){
             if(btAdapter.isDiscovering()){
