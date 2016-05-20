7d568d4dc1009842550ea155d2c0bd6539849813
Nils Dralle <nils.91@googlemail.com>
2016-05-20 13:20:28 +0200

    [BluetoothTest] added support for Android 6.0
    request some permissions at runtime
    
    Signed-off-by: Nils Dralle <nils.91@googlemail.com>

--- BluetoothTest/app/src/main/java/de/dralle/bluetoothtest/MainActivity.java
+++ BluetoothTest/app/src/main/java/de/dralle/bluetoothtest/MainActivity.java
@@ -1,18 +1,23 @@
 package de.dralle.bluetoothtest;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.database.DataSetObserver;
 import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListAdapter;
@@ -23,9 +28,11 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String LOG_TAG=MainActivity.class.getName();
+    private static final String LOG_TAG = MainActivity.class.getName();
 
-    private int REQUEST_ENABLE_BT=42;
+    private final int REQUEST_ENABLE_BT = 42;
+    private final int REQUEST_ACCESS_COARSE_LOCATION=1;
+
     private ArrayList<BluetoothDevice> devices;
     private ArrayList<String> deviceNames;
     private ArrayAdapter<String> displayAdapter;
@@ -35,32 +42,33 @@ public class MainActivity extends AppCompatActivity {
             String action = intent.getAction();
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                Log.i(LOG_TAG,"New device found");
-                Log.i(LOG_TAG,device.getAddress());
-                Log.i(LOG_TAG,device.getName());
-                Log.i(LOG_TAG,device.getBluetoothClass().toString());
-                Log.i(LOG_TAG,"Bond state: "+device.getBondState()+"");
-                Log.i(LOG_TAG,"Type: "+device.getBondState()+"");
+                Log.i(LOG_TAG, "New device found");
+                Log.i(LOG_TAG, device.getAddress());
+                Log.i(LOG_TAG, device.getName());
+                Log.i(LOG_TAG, device.getBluetoothClass().toString());
+                Log.i(LOG_TAG, "Bond state: " + device.getBondState() + "");
+                Log.i(LOG_TAG, "Type: " + device.getBondState() + "");
                 devices.add(device);
                 deviceNames.add(device.getName());
                 displayAdapter.notifyDataSetChanged();
             }
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        devices=new ArrayList<>();
-        deviceNames=new ArrayList<>();
-        displayAdapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,deviceNames){
+        devices = new ArrayList<>();
+        deviceNames = new ArrayList<>();
+        displayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, deviceNames) {
             @Override
             public View getView(int position, View convertView,
                                 ViewGroup parent) {
-                View view =super.getView(position, convertView, parent);
+                View view = super.getView(position, convertView, parent);
 
-                TextView textView=(TextView) view.findViewById(android.R.id.text1);
+                TextView textView = (TextView) view.findViewById(android.R.id.text1);
 
                 textView.setTextColor(Color.BLUE);
 
@@ -68,99 +76,112 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        Button btnScan=(Button)findViewById(R.id.btnScn);
+        Button btnScan = (Button) findViewById(R.id.btnScn);
         btnScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startDeviceScan();
             }
         });
-        ListView lvDevices=(ListView)findViewById(R.id.listViewDevices);
+        ListView lvDevices = (ListView) findViewById(R.id.listViewDevices);
         lvDevices.setAdapter(displayAdapter);
 
+        lvDevices.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i(LOG_TAG, "Clicked item " + id);
+            }
+        });
 
-
-    }
-    protected void onStart(){
-        super.onStart();
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         registerReceiver(broadcastReceiver, filter); // Don't forget to unregister during onDestroy
 
 
-        BluetoothAdapter btAdapter=BluetoothAdapter.getDefaultAdapter();
-        if(btAdapter!=null){
-            if(!btAdapter.isEnabled()){
-                Log.e(LOG_TAG,"Bluetooth not enabled");
-                Toast.makeText(getApplicationContext(),getText(R.string.btNotEnabled),Toast.LENGTH_LONG).show();
-
-                Intent btOnRequest = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(btOnRequest, REQUEST_ENABLE_BT);
-            }else{
-                startDeviceScan();
-            }
-        }else{
-            Log.e(LOG_TAG,"Bluetooth not supported");
-            Toast.makeText(getApplicationContext(),getText(R.string.btNotSupported),Toast.LENGTH_LONG).show();
-            finish();
-        }
     }
 
-    protected void onRestart(){
+    protected void onStart() {
+        super.onStart();
+
+
+
+
+
+    }
+
+    protected void onRestart() {
         super.onRestart();
     }
 
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
+        startDeviceScan();
     }
 
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
     }
 
-    protected void onStop(){
+    protected void onStop() {
         super.onStop();
     }
 
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver(broadcastReceiver);
 
-        BluetoothAdapter btAdapter=BluetoothAdapter.getDefaultAdapter();
-        if(btAdapter!=null&&btAdapter.isEnabled()){
-            if(btAdapter.isDiscovering()){
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (btAdapter != null && btAdapter.isEnabled()) {
+            if (btAdapter.isDiscovering()) {
                 Log.w(LOG_TAG, "Discovery running. Cancelling discovery");
                 btAdapter.cancelDiscovery();
             }
         }
     }
+
     @Override
-    protected void onActivityResult (int requestCode,
-                                int resultCode,
-                                Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-        if(requestCode==REQUEST_ENABLE_BT){
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT) {
             startDeviceScan();
         }
     }
+
     private void startDeviceScan() {
-        BluetoothAdapter btAdapter=BluetoothAdapter.getDefaultAdapter();
-        if(btAdapter!=null&&btAdapter.isEnabled()){
-            if(btAdapter.isDiscovering()){
-                Log.w(LOG_TAG, "Discovery running. Cancelling discovery");
-                btAdapter.cancelDiscovery();
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION.toString())!= PackageManager.PERMISSION_GRANTED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION.toString()},REQUEST_ACCESS_COARSE_LOCATION); //need to request permission at runtime for android 6.0+
             }
-            Log.i(LOG_TAG,"Starting discovery");
-            if(btAdapter.startDiscovery()){
-                Log.i(LOG_TAG,"Started discovery");
-                devices.clear();
-                deviceNames.clear();
-                displayAdapter.notifyDataSetChanged();
-            }else{
-                Log.i(LOG_TAG,"Failed discovery");
+        }
+
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (btAdapter != null) {
+            if (btAdapter.isEnabled()) {
+
+
+                if (btAdapter.isDiscovering()) {
+                    Log.w(LOG_TAG, "Discovery running. Cancelling discovery");
+                    btAdapter.cancelDiscovery();
+                }
+                Log.i(LOG_TAG, "Starting discovery");
+                if (btAdapter.startDiscovery()) {
+                    Log.i(LOG_TAG, "Started discovery");
+                    devices.clear();
+                    deviceNames.clear();
+                    displayAdapter.notifyDataSetChanged();
+                } else {
+                    Log.i(LOG_TAG, "Failed discovery");
+                }
+            } else {
+                Log.i(LOG_TAG, "Bluetooth disabled");
+
+                Intent btOnRequest = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(btOnRequest, REQUEST_ENABLE_BT);
             }
-        }else{
-            Log.e(LOG_TAG,"Bluetooth not enabled");
-            Toast.makeText(getApplicationContext(),getText(R.string.btNotEnabled),Toast.LENGTH_LONG).show();
+        } else {
+            Log.e(LOG_TAG, "Bluetooth not enabled");
+            Toast.makeText(getApplicationContext(), getText(R.string.btNotEnabled), Toast.LENGTH_LONG).show();
             finish();
         }
     }
