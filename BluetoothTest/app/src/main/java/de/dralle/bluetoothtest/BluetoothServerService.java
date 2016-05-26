144fe4535620f7643220ce975415e286db9a4011
Nils Dralle <nils.91@googlemail.com>
2016-05-26 10:59:27 +0200

    [BluetoothTest] added Background service
    Signed-off-by: Nils Dralle <nils.91@googlemail.com>

--- BluetoothTest/app/src/main/java/de/dralle/bluetoothtest/BluetoothServerService.java
+++ BluetoothTest/app/src/main/java/de/dralle/bluetoothtest/BluetoothServerService.java
@@ -1,7 +1,17 @@
 package de.dralle.bluetoothtest;
 
+import android.app.IntentService;
+import android.content.Intent;
+
 /**
  * Created by Nils on 26.05.2016.
  */
-public class BluetoothServerService {
+public class BluetoothServerService extends IntentService {
+    public BluetoothServerService(){
+        super("BluetoothServerService");
+    }
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String data=intent.getDataString();
+    }
 }
