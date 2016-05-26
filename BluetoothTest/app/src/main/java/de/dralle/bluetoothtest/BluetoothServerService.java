326eca3b21bdf423bce79d59650ce080222a8633
Nils Dralle <nils.91@googlemail.com>
2016-05-26 12:06:06 +0200

    [BluetoothTest] added logger to intent service
    Signed-off-by: Nils Dralle <nils.91@googlemail.com>

--- BluetoothTest/app/src/main/java/de/dralle/bluetoothtest/BluetoothServerService.java
+++ BluetoothTest/app/src/main/java/de/dralle/bluetoothtest/BluetoothServerService.java
@@ -2,6 +2,7 @@ package de.dralle.bluetoothtest;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 /**
  * Created by Nils on 26.05.2016.
@@ -13,6 +14,8 @@ public class BluetoothServerService extends IntentService {
     }
     @Override
     protected void onHandleIntent(Intent intent) {
+        Log.i(LOG_TAG,"New work request");
         String data=intent.getDataString();
+        Log.i(LOG_TAG,"Data: "+data);
     }
 }
