9eb9a0d63a36ca4dbebabc570372c396edb26849
Nils Dralle <nils.91@googlemail.com>
2016-06-01 10:15:21 +0200

    [BluetoothTest] request permission
    Signed-off-by: Nils Dralle <nils.91@googlemail.com>

--- BluetoothTest/app/src/main/java/de/dralle/bluetoothtest/GUI/SPMAServiceConnector.java
+++ BluetoothTest/app/src/main/java/de/dralle/bluetoothtest/GUI/SPMAServiceConnector.java
@@ -1,5 +1,6 @@
 package de.dralle.bluetoothtest.GUI;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.Service;
@@ -10,8 +11,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.IBinder;
 import android.os.Messenger;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 import org.json.JSONException;
@@ -28,6 +32,7 @@ import de.dralle.bluetoothtest.BGS.deprecated.BluetoothServerService;
 public class SPMAServiceConnector {
     private static final String LOG_TAG = SPMAServiceConnector.class.getName();
     public static final String ACTION_NEW_MSG = "SPMAServiceConnector.ACTION_NEW_MSG";
+    private final int REQUEST_ACCESS_COARSE_LOCATION = 1;
     private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
@@ -144,6 +149,15 @@ public class SPMAServiceConnector {
      * @return true if service is running and message was sent
      */
     public boolean scanForNearbyDevices() {
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(parentActivity, Manifest.permission.ACCESS_COARSE_LOCATION.toString()) != PackageManager.PERMISSION_GRANTED) {
+
+                parentActivity.requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION.toString()},REQUEST_ACCESS_COARSE_LOCATION); //need to request permission at runtime for android 6.0+
+
+            }
+        }
+
         if(isServiceRunning()){
             Log.i(LOG_TAG,"Service is running. Sending Scan");
             JSONObject mdvCmd = new JSONObject();
