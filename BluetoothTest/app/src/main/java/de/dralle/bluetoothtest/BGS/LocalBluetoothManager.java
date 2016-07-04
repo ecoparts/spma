9c6aee370f784dfad2a963b6f29ff3d0ae9b7396
Nils Dralle <nils.91@googlemail.com>
2016-07-04 12:46:14 +0200

    [BTTest] formatting
    Signed-off-by: Nils Dralle <nils.91@googlemail.com>

--- BluetoothTest/app/src/main/java/de/dralle/bluetoothtest/BGS/LocalBluetoothManager.java
+++ BluetoothTest/app/src/main/java/de/dralle/bluetoothtest/BGS/LocalBluetoothManager.java
@@ -84,13 +84,11 @@ public class LocalBluetoothManager {
     public boolean scanForNearbyDevices(JSONObject msgData) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION.toString()) == PackageManager.PERMISSION_GRANTED) {
-
                 Log.i(LOG_TAG, "Permission ACCESS_COARSE_LOCATION granted");
             } else {
                 Log.w(LOG_TAG, "Permission ACCESS_COARSE_LOCATION denied");
                 return false;
             }
-
         }
         BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
         if (btAdapter != null) {
