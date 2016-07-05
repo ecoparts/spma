b0c5aa749b0828fb65bab2f3a3ea51a144e259c0
Nils Dralle <nils.91@googlemail.com>
2016-07-05 14:28:05 +0200

    [BTTest] friends fragment
    Signed-off-by: Nils Dralle <nils.91@googlemail.com>

--- BluetoothTest/app/src/main/java/de/dralle/bluetoothtest/GUI/TwoFragment.java
+++ BluetoothTest/app/src/main/java/de/dralle/bluetoothtest/GUI/TwoFragment.java
@@ -1,108 +1,171 @@
 package de.dralle.bluetoothtest.GUI;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import de.dralle.bluetoothtest.GUI.ToStringAdapters.Connection;
 import de.dralle.bluetoothtest.R;
 
-
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link TwoFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link TwoFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Created by Niklas on 18.06.2016.
  */
 public class TwoFragment extends Fragment {
-    /*// TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String LOG_TAG = TwoFragment.class.getName();
+    private SPMAServiceConnector serviceConnector;
+    public static final String ACTION_NEW_MSG = "TwoFragment.ACTION_NEW_MSG";
 
-    private OnFragmentInteractionListener mListener;*/
+    private ArrayList<Connection> cachedConnections;
+    private ArrayAdapter<Connection> displayAdapter;
+
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (ACTION_NEW_MSG.equals(action)) {
+                String msg = intent.getStringExtra("msg");
+                Log.i(LOG_TAG, "New message");
+                Log.i(LOG_TAG, msg);
+                JSONObject msgData = null;
+                try {
+                    msgData = new JSONObject(msg);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                if (msgData != null) {
+                    if (serviceConnector.checkMessage(msgData)) {
+                        switch (serviceConnector.getMessageAction(msgData)) {
+                            case "FriendDevice":
+                                Connection c = new Connection();
+                                try {
+                                    if(msgData.getBoolean("Active")){
+                                        c.setConnectionName(msgData.getString("Name"));
+                                        c.setConnectionTargetDeviceAddress(msgData.getString("Address"));
+                                        c.setLastSeen(msgData.getInt("LastSeen"));
+                                        c.setSecure(msgData.getBoolean("Secure"));
+                                        cachedConnections.add(c);
+                                        Log.i(LOG_TAG,"Connection added "+c);
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                displayAdapter.notifyDataSetChanged();
+                                Log.v(LOG_TAG,"Connection added "+"Dataset changed");
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                } else {
+                    Log.w(LOG_TAG, "Message not JSON");
+                }
+            }
+        }
+    };
 
     public TwoFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TwoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    /*public static TwoFragment newInstance(String param1, String param2) {
-        TwoFragment fragment = new TwoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }*/
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        getActivity().unregisterReceiver(broadcastReceiver);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        IntentFilter filter = new IntentFilter(ACTION_NEW_MSG);
+        getActivity().registerReceiver(broadcastReceiver, filter);
+
+        serviceConnector.requestCachedConnections();
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }*/
+
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_two, container, false);
     }
 
-    /*// TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        View v = getView();
+
+
+        serviceConnector = SPMAServiceConnector.getInstance(getActivity());
+
+        if (serviceConnector.isServiceRunning()) {
+            Log.i(LOG_TAG, "Service already running");
         } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+            Log.v(LOG_TAG, "Service starting");
+            serviceConnector.startService();
         }
+        serviceConnector.selectUser(0); //always select user 0, since its the default one
+
+        cachedConnections = new ArrayList<>();
+        displayAdapter = new ArrayAdapter<Connection>(getActivity(), android.R.layout.simple_list_item_1, cachedConnections) {
+            @Override
+            public View getView(int position, View convertView,
+                                ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+
+                TextView textView = (TextView) view.findViewById(android.R.id.text1);
+
+                textView.setTextColor(Color.BLUE);
+
+                return view;
+            }
+        };
+
+        //cached connections
+        ListView lvDevices = (ListView) getView().findViewById(R.id.listViewFriendDevices);
+        lvDevices.setAdapter(displayAdapter);
+
+        lvDevices.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Connection c=cachedConnections.get((int) id);
+                if(c!=null){
+                    serviceConnector.startChatActvity(c.getConnectionTargetDeviceAddress());
+                }
+
+            }
+        });
+
+
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
-    *//**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     *//*
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }*/
 }
